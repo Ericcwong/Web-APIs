86f268c6a11fc552aa622a660ca0ca93283c9caa
Eric Wong <ericwong12@live.com>
2019-12-09 11:43:19 -0700

    Added content index.html added bootstrap layout style.css has been added questions.js 5 questions have been added to the page

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+/* upper left hand side needs a way to view the highscore
+after the game they have an option to view and clear the highscores  */
+
+
+
+
+
+
+/*The center of the page is where the quiz will sit. Need a way to pull questions from questions.js*/
+//Will need buttons for starting the quiz and buttons for when the user has made their selection.
+
+
+
+
+
+//top right corner has a timer that starts when user hits the start quiz button
---
+++ javascript/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Who is not part of the Avengers?",
+        choices: ["SpiderMan","Batman","Thor","IronMan"],
+        answer: "Batman"
+    },
+    {
+        title:"What is the Highest Grossing film of all time? ",
+        choices:["Avengers: Endgame", "Titanic","Avatar", "Furious 7"],
+        answer:"Avengers: Endgame"
+    },
+    {
+        title:"What sauce originated in Utah",
+        choices:["Ketchup", "Mayonnaise", "Catsup", "Fry-Sauce"],
+        answer:"Fry-Sauce"
+    },
+    {
+        title:"What is the largest animal currently living in planet Earth",
+        choices:["Tyannosaurus Rex","Elephant","Blue Whale","Terry Crews"],
+        answer:"Blue Whale"
+    },
+    {
+        title:"What is the Average student loan debt in 2018",
+        choices:["$10,000", "$20,000", "$30,000","$40,000"],
+        answer:"$30,000"
+    }
+
+]
