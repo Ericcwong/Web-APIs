7ba13309fc0d06c209cd33a55ae1d4f881f332c7
Eric Wong <ericwong12@live.com>
2019-12-11 16:55:54 -0700

    pseudo code file added to help with breaking down the program

---
+++ javascript/pseudocode.js
@@ -0,0 +1,15 @@
+//Onclick function that starts the quiz
+
+
+
+//Once quiz starts, User prompted with the question and followed with 4 buttons
+    
+    //The four buttons generated must have some sort of data value to it
+
+
+    /*When the user clicks on a button, must take an if statement to check if the two strings are the same.
+      meaning they are correct and to let the user know they got it correct insert a new div saying, "You are correct!". 
+      If the two strings are incorrect use the same div as correct but replace it with it saying,"You are incorrect, try again!"*/
+
+    /*When the user chooses the correct answer they move onto the next question. That is when the for loop would come in. Or say
+      if the user has not clicked */
