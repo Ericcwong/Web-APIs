aeda3a231b7e390af2b47de1876ff6ccb90c4dc3
Eric Wong <ericwong12@live.com>
2019-12-16 17:34:07 -0700

    Major update index.html removed hard coded quiz style changed styles index.js simplified it questions.js made it print the questions under a div checking if the question is right does not work

--- javascript/index.js
+++ javascript/index.js
@@ -14,42 +14,19 @@ after the game they have an option to view and clear the highscores  */
 //Onclick start quiz, also start the timer function then jumps to a new page
     // startQuiz.addEventListener("click", addtimer);
     // $("#startQuizBtn").click(addtimer);
-    document.getElementById("startQuizBtn").addEventListener("click",function(){
-        document.getElementById("quiz").hidden = true;
-        document.getElementById("quizGame").hidden = false;
+    // document.getElementById("startQuizBtn").addEventListener("click",function(){
+    //     document.getElementById("quiz").hidden = true;
+    //     document.getElementById("quizGame").hidden = false;
+    //     displayQuestion();
+    //     // addtimer();
+    // }, false);
+    $("#startQuizBtn").on("click", function(){
+        $("#quiz").hide();
+        $("#quizGame").show();
         displayQuestion();
-        addtimer();
-    }, false);
+    });
+    
 
-    i=0;
-    var quizQuestion = document.querySelector(".title");
-    var answer0 = document.querySelector("#answer0");
-    var answer1 = document.querySelector("#answer1");
-    var answer2 = document.querySelector("#answer2");
-    var answer3 = document.querySelector("#answer3");
-    var answers = false; 
-   function displayQuestion(){
-       if(i >= questions.length){
-       
-       }
-    var choices = questions[i].choices;
-    for(var c = 0; c < choices.length; c++){
-        document.getElementById("answer" + c).textContent = choices[c];
-        // debugger;
-    }
-     quizQuestion.textContent = questions[i].title;
-    }
-    var correctAnswer = questions[i].answer;
-    function answerChecker(){
-        if(document.querySelectorAll("button").innerHTML !== correctAnswer){
-            alert("wrong");
-        }else {alert("right");}
-    }
-
-   document.getElementById("buttons").addEventListener("click",function(){  
-    displayQuestion();
-    answerChecker();
-   });
 
 /*top right corner has a timer that starts when user hits the start quiz button 
 (This would be inside the onclick quiz function)*/
@@ -57,21 +34,21 @@ after the game they have an option to view and clear the highscores  */
 //variables for the timer
 
 
-function addtimer(){
-    var baseTimer = document.getElementById("timer");
-    var startTime = 76;
-    //This is to add element h4
+// function addtimer(){
+//     var baseTimer = document.getElementById("timer");
+//     var startTime = 76;
+//     //This is to add element h4
     
-    displayTime = document.createElement("h4");
-        //This is the actual number count down
-        var timerInterval = setInterval(function(){
-        startTime--;
-        displayTime.textContent = "Timer: " + startTime;
-        baseTimer.append(displayTime);
+//     displayTime = document.createElement("h4");
+//         //This is the actual number count down
+//         var timerInterval = setInterval(function(){
+//         startTime--;
+//         displayTime.textContent = "Timer: " + startTime;
+//         baseTimer.append(displayTime);
         
-        if(startTime === 0){
-            clearInterval(timerInterval)
-            alert("Game Over ")
-        }
-            }, 1000);
-}
+//         if(startTime === 0){
+//             clearInterval(timerInterval)
+//             alert("Game Over ")
+//         }
+//             }, 1000);
+// }
--- javascript/questions.js
+++ javascript/questions.js
@@ -1,27 +1,53 @@
-    var questions = [
+var quiz = {
+    questions: [
         {
             title: "Who is not part of the Avengers?",
             choices: ["SpiderMan","Batman","Thor","IronMan"],
             answer: "Batman"
         },
         {
-            title:"What is the Highest Grossing film of all time? ",
+            title:"What is the Highest Grossing film of all time?",
             choices:["Avengers: Endgame", "Titanic","Avatar", "Furious 7"],
-            answer:"Avengers: Endgame"
+            answer: 0
         },
         {
             title:"What sauce originated in Utah",
             choices:["Ketchup", "Mayonnaise", "Catsup", "Fry-Sauce"],
-            answer:"Fry-Sauce"
+            answer: 3
         },
         {
             title:"What is the largest animal currently living in planet Earth",
             choices:["Tyannosaurus Rex","Elephant","Blue Whale","Terry Crews"],
-            answer:"Blue Whale"
+            answer: 2
         },
         {
             title:"What is the Average student loan debt in 2018",
             choices:["$10,000", "$20,000", "$30,000","$40,000"],
-            answer:"$30,000"
-        }
-    ]
+            answer: 2
+        },
+    ],
+    questionCounter: 0
+}
+var currentQuestion = quiz.questions[quiz.questionCounter];
+
+function displayQuestion(){
+    $("#questions").html(`<h3>${currentQuestion.title}</h3>`)
+
+    for(var i =0; i < currentQuestion.choices.length; i++){
+        $("#answerButtons").append(`
+            <button id="answer-${i+1}" value=${currentQuestion.choices[i]}>
+            ${currentQuestion.choices[i]}
+            </button>
+        `)
+    }
+}
+$(document).on("click", "#answerButtons",function(){
+    var currentQuestion = quiz.questions[quiz.questionCounter];
+    var choice = $(this).val();
+    if(choice === currentQuestion.answer){
+        alert("well done");
+    }
+    // else{
+    //     alert("you are wrong");
+    // }
+});
