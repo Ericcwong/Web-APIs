164ae52f6f1815610c0f9a07b06bb2856e3d20a1
Eric Wong <ericwong12@live.com>
2019-12-18 11:23:12 -0700

    Code is working now but needs styling index.html added div class right or wrong questions.js has been overhauled and everything about it has been changed

--- javascript/questions.js
+++ javascript/questions.js
@@ -1,3 +1,4 @@
+
 var quiz = {
     questions: [
         {
@@ -7,47 +8,62 @@ var quiz = {
         },
         {
             title:"What is the Highest Grossing film of all time?",
-            choices:["Avengers: Endgame", "Titanic","Avatar", "Furious 7"],
-            answer: 0
+            choices:["Avengers:Endgame", "Titanic","Avatar", "Furious 7"],
+            answer: "Avengers:Endgame"
         },
         {
             title:"What sauce originated in Utah",
             choices:["Ketchup", "Mayonnaise", "Catsup", "Fry-Sauce"],
-            answer: 3
+            answer: "Fry-Sauce"
         },
         {
             title:"What is the largest animal currently living in planet Earth",
             choices:["Tyannosaurus Rex","Elephant","Blue Whale","Terry Crews"],
-            answer: 2
+            answer: "Blue Whale"
         },
         {
             title:"What is the Average student loan debt in 2018",
             choices:["$10,000", "$20,000", "$30,000","$40,000"],
-            answer: 2
+            answer: "$30,000"
         },
     ],
     questionCounter: 0
 }
-var currentQuestion = quiz.questions[quiz.questionCounter];
 
+console.log(quiz);
+if(quiz.questionCounter === 5){
+    $("#questions").append(`<h2>You did it!</h2>`)
+}
 function displayQuestion(){
-    $("#questions").html(`<h3>${currentQuestion.title}</h3>`)
+    var currentQuestion = quiz.questions[quiz.questionCounter];
+    
+    $("#answerButtons").empty();
+    $("#questions").html(`<h3>${currentQuestion.title}</h3>`);
 
     for(var i =0; i < currentQuestion.choices.length; i++){
+        
         $("#answerButtons").append(`
-            <button id="answer-${i+1}" value=${currentQuestion.choices[i]}>
+            <button id="answer-${i+1}" class="answerButton" value=${currentQuestion.choices[i]}>
             ${currentQuestion.choices[i]}
-            </button>
-        `)
+            </button>   
+        `)    
     }
+    console.log($("#answerButtons"))
 }
-$(document).on("click", "#answerButtons",function(){
+$(document).on("click",".answerButton",function(){
+    console.log("click", ".answerButton")
     var currentQuestion = quiz.questions[quiz.questionCounter];
     var choice = $(this).val();
+    console.log(choice);
     if(choice === currentQuestion.answer){
-        alert("well done");
+        quiz.questionCounter++;
+        $(".rightOrWrong").html("You are Right!!");
+        // $(".answerButton").empty();
+        displayQuestion();
+        // $(".rightOrWrong").empty();
+    }else{
+        $(".rightorWrong").html("You are Wrong")
+        quiz.questionCounter++;
+
     }
-    // else{
-    //     alert("you are wrong");
-    // }
 });
