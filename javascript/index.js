596b59dd24ceb8a9cc8f8afb6f8d6c3f40459002
Eric Wong <ericwong12@live.com>
2019-12-18 18:30:39 -0700

    Questions.js is almost working needs game over screen

--- javascript/index.js
+++ javascript/index.js
@@ -22,33 +22,37 @@ after the game they have an option to view and clear the highscores  */
     // }, false);
     $("#startQuizBtn").on("click", function(){
         $("#quiz").hide();
+        $(".gameOver").hide();
         $("#quizGame").show();
         displayQuestion();
+        addtimer();
+    });
+    $("#restart").on("click", function(){
+        $(".gameOver").hide();
+        $("#quizGame").show();
+        displayQuestion();
+        var counter=setInterval(timer, 1000);
     });
     
 
-
 /*top right corner has a timer that starts when user hits the start quiz button 
 (This would be inside the onclick quiz function)*/
-
-//variables for the timer
-
-
-// function addtimer(){
-//     var baseTimer = document.getElementById("timer");
-//     var startTime = 76;
-//     //This is to add element h4
+var startTime = 75;
+function addtimer(){
+    var baseTimer = document.getElementById("timer");
     
-//     displayTime = document.createElement("h4");
-//         //This is the actual number count down
-//         var timerInterval = setInterval(function(){
-//         startTime--;
-//         displayTime.textContent = "Timer: " + startTime;
-//         baseTimer.append(displayTime);
+    //This is to add element h4
+    
+    displayTime = document.createElement("h4");
+        //This is the actual number count down
+        var timerInterval = setInterval(function(){
+        startTime--;
+        displayTime.textContent = "Timer: " + startTime;
+        baseTimer.append(displayTime);
         
-//         if(startTime === 0){
-//             clearInterval(timerInterval)
-//             alert("Game Over ")
-//         }
-//             }, 1000);
-// }
+        if(startTime === 0){
+            clearInterval(timerInterval)
+            gameOver();
+        }
+            }, 1000);
+        }
--- javascript/questions.js
+++ javascript/questions.js
@@ -1,69 +1,76 @@
-
 var quiz = {
-    questions: [
-        {
-            title: "Who is not part of the Avengers?",
-            choices: ["SpiderMan","Batman","Thor","IronMan"],
-            answer: "Batman"
-        },
-        {
-            title:"What is the Highest Grossing film of all time?",
-            choices:["Avengers:Endgame", "Titanic","Avatar", "Furious 7"],
-            answer: "Avengers:Endgame"
-        },
-        {
-            title:"What sauce originated in Utah",
-            choices:["Ketchup", "Mayonnaise", "Catsup", "Fry-Sauce"],
-            answer: "Fry-Sauce"
-        },
-        {
-            title:"What is the largest animal currently living in planet Earth",
-            choices:["Tyannosaurus Rex","Elephant","Blue Whale","Terry Crews"],
-            answer: "Blue Whale"
-        },
-        {
-            title:"What is the Average student loan debt in 2018",
-            choices:["$10,000", "$20,000", "$30,000","$40,000"],
-            answer: "$30,000"
-        },
-    ],
-    questionCounter: 0
-}
+  questions: [
+    {
+      title: "Who is not part of the Avengers?",
+      choices: ["SpiderMan", "Batman", "Thor", "IronMan"],
+      answer: "Batman"
+    },
+    {
+      title: "What is the Highest Grossing film of all time?",
+      choices: ["Avengers:Endgame", "Titanic", "Avatar", "Furious 7"],
+      answer: "Avengers:Endgame"
+    },
+    {
+      title: "What sauce originated in Utah",
+      choices: ["Ketchup", "Mayonnaise", "Catsup", "Fry-Sauce"],
+      answer: "Fry-Sauce"
+    },
+    {
+      title: "What is the largest animal currently living in planet Earth",
+      choices: ["Tyannosaurus Rex", "Elephant", "BlueWhale", "Terry Crews"],
+      answer: "BlueWhale"
+    },
+    {
+      title: "What is the Average student loan debt in 2018",
+      choices: ["$10,000", "$20,000", "$30,000", "$40,000"],
+      answer: "$30,000"
+    }
+  ],
+  questionCounter: 0
+};
 
 console.log(quiz);
-if(quiz.questionCounter === 5){
-    $("#questions").append(`<h2>You did it!</h2>`)
-}
-function displayQuestion(){
-    var currentQuestion = quiz.questions[quiz.questionCounter];
-    
-    $("#answerButtons").empty();
-    $("#questions").html(`<h3>${currentQuestion.title}</h3>`);
+var score = 0;
 
-    for(var i =0; i < currentQuestion.choices.length; i++){
-        
-        $("#answerButtons").append(`
-            <button id="answer-${i+1}" class="answerButton" value=${currentQuestion.choices[i]}>
+function displayQuestion() {
+  var currentQuestion = quiz.questions[quiz.questionCounter];
+  $("#scoreBoard").html(`<h2>Score: ${score}</h2>`);
+  $("#answerButtons").empty();
+  $("#questions").html(`<h3>${currentQuestion.title}</h3>`);
+
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    $("#answerButtons").append(`
+            <button id="answer-${i + 1}" class="answerButton" value=${
+      currentQuestion.choices[i]
+    }>
             ${currentQuestion.choices[i]}
             </button>   
-        `)    
-    }
-    console.log($("#answerButtons"))
+        `);
+  }
 }
-$(document).on("click",".answerButton",function(){
-    console.log("click", ".answerButton")
-    var currentQuestion = quiz.questions[quiz.questionCounter];
-    var choice = $(this).val();
-    console.log(choice);
-    if(choice === currentQuestion.answer){
-        quiz.questionCounter++;
-        $(".rightOrWrong").html("You are Right!!");
-        // $(".answerButton").empty();
-        displayQuestion();
-        // $(".rightOrWrong").empty();
-    }else{
-        $(".rightorWrong").html("You are Wrong")
-        quiz.questionCounter++;
-
-    }
+$(document).on("click", ".answerButton", function() {
+  console.log("click", ".answerButton");
+  var currentQuestion = quiz.questions[quiz.questionCounter];
+  var choice = $(this).val();
+  console.log(choice);
+  if (choice === currentQuestion.answer) {
+    quiz.questionCounter++;
+    score++;
+    $(".rightOrWrong").html("You are Right!!");
+    displayQuestion();
+  } else {
+    $(".rightOrWrong").html("You are Wrong");
+    startTime-15;
+  }
 });
+
+function gameOver(){
+    $("#quizGame").hide();
+     $(".gameOver").show();
+    $(".gameOver").append(`
+    <h2>Game Over</h2>
+    <h2>${score}</h2>
+    <button onClick="window.location.reload();">Start Again!</button>
+`)
+     
+}
